3aaefdd8e262492e9913014753e8df765d4d27e7
Mateus Almeida Bissonho da Silva <mateusbissonho@gmail.com>
2018-08-02 22:40:16 -0300

    Models construídos.

--- app/src/main/java/br/edu/iff/pooa20181/trabalho03_2018_1/model/Mecanico.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho03_2018_1/model/Mecanico.java
@@ -2,6 +2,66 @@ package br.edu.iff.pooa20181.trabalho03_2018_1.model;
 
 public class Mecanico {
 
+    private String nome, rua, bairro, dataDeNascimento, municipio;
+    private Double latitude, longitude;
 
+    public Mecanico(){
 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
--- app/src/main/java/br/edu/iff/pooa20181/trabalho03_2018_1/model/Oficina.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho03_2018_1/model/Oficina.java
@@ -1,4 +1,59 @@
 package br.edu.iff.pooa20181.trabalho03_2018_1.model;
 
 public class Oficina {
+
+    private String nome, rua, bairro, municipio;
+    private Double latitude, longitude;
+
+    public Oficina(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
