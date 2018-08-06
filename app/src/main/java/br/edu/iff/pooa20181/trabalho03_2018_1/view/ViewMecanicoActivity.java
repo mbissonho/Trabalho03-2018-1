951209a55687d63c1b12d5bb9119d97d6766509f
Mateus Almeida Bissonho da Silva <mateusbissonho@gmail.com>
2018-08-06 08:51:03 -0300

    Interações na activity de detalhes do mecânico construídas.

--- app/src/main/java/br/edu/iff/pooa20181/trabalho03_2018_1/view/ViewMecanicoActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho03_2018_1/view/ViewMecanicoActivity.java
@@ -3,15 +3,14 @@ package br.edu.iff.pooa20181.trabalho03_2018_1.view;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ZoomControls;
@@ -28,15 +27,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-
 import br.edu.iff.pooa20181.trabalho03_2018_1.R;
 import br.edu.iff.pooa20181.trabalho03_2018_1.model.Mecanico;
 import io.realm.Realm;
 
 public class ViewMecanicoActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private Button toggle, ret;
     private GoogleMap map;
     ZoomControls zoomCtrl;
     Double deviceLat = null, deviceLon = null;
@@ -88,6 +85,45 @@ public class ViewMecanicoActivity extends FragmentActivity implements OnMapReady
             }
         };
 
+        this.zoomCtrl = findViewById(R.id.zoomCtrl);
+        this.zoomCtrl.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map.animateCamera(CameraUpdateFactory.zoomOut());
+            }
+        });
+
+        this.zoomCtrl.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map.animateCamera(CameraUpdateFactory.zoomIn());
+
+            }
+        });
+
+
+        this.toggle = findViewById(R.id.btnToggle);
+        this.toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (map.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+                    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                    toggle.setText("NORM");
+                } else {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    toggle.setText("SAT");
+                }
+
+            }
+        });
+
+        this.ret = findViewById(R.id.btnRet);
+        this.ret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewMecanicoActivity.this.setLocal(ViewMecanicoActivity.this.mecanico);
+            }
+        });
 
         this.bindAndSet();
     }
